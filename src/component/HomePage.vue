a25e96c813f148d22080ee3b1dcef6db71794253
zisehuiyi2007 <rosiwang@163.com>
2018-06-27 21:29:16 +0800

    清理代码 保留基本显示页

--- src/component/HomePage.vue
+++ src/component/HomePage.vue
@@ -1,24 +1,8 @@
 <template>
     <div>
-        <div id="slider" class="nivoSlider">
-            <p>
-                测试测试测试888888888888888888
-            </p>
-        </div>
-
-        <header>
-            <a class="menu-icon">
-                <span></span>
-            </a>
-            <nav class="menu visuallyhidden">
-                <ul>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#about">About me</a></li>
-                    <li><a href="#work">My Works</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                </ul>
-            </nav>
-        </header>
+        <!--<header>-->
+            <!---->
+        <!--</header>-->
 
         <!--<div id="slider" class="nivoSlider">-->
             <!--<img src="source/display0.jpg"/>-->
@@ -26,160 +10,63 @@
             <!--<img src="source/display1.jpg"/>-->
         <!--</div>-->
 
-        <section id="home">
-                <div class="overlay">
-                    <h1>Rosi Blogs</h1>
-                    <h2>UI / UX / Frontend</h2>
-                    <a href="#" target="_blank" class="but">Download Resume</a>
-                </div>
-        </section>
+        <!--<section id="home">-->
+                <!--<div class="overlay">-->
+                    <!--<h1>Rosi Blogs</h1>-->
+                    <!--<h2>UI / UX / Frontend</h2>-->
+                    <!--<a href="#" target="_blank" class="but">Download Resume</a>-->
+                <!--</div>-->
+        <!--</section>-->
+        <img style="width: 100%;height: 500px" src="static/img/header-bg.jpg"/>
 
         <!--about me-->
-        <section id="about" class="row">
-            <div class="page about-content">
-                <div class="col about-title">
+        <section style="text-align: center" id="about" >
+            <div >
+                <div >
                     <h3
                             data-100="-webkit-transform: translateX(100px); opacity:0;"
                             data-500="-webkit-transform: translateX(0px); opacity:1;"
                     >About me</h3>
                 </div>
-                <div class="col empty">&nbsp;</div>
-                <div class="col about-description">
-                    <p
-                            data-100="opacity:0;"
-                            data-500="opacity:1;"
-                    >My name is Rosi.I like travelling, reading and painting.
+                <div style="font-size: 20px">
+                    My name is Rosi.I like travelling, reading and painting.
                         I think of myself as an optimistic, enthusiasm towards life,
                         infinite hope for the future,
                         I like me.</p>
                     <a href="#" class="but">Get in touch</a>
                 </div>
-                <div class="col empty">&nbsp;</div>
+
             </div>
         </section>
 
    <!--展示区-->
-        <section id="work" class="row">
-            <div class="work-content page">
-                <div class="col work-title">
-                    <h3
-                            data-900="-webkit-transform: translateX(-100px); opacity:0;"
-                            data-1300="-webkit-transform: translateX(0px); opacity:1;"
-                    >My latest work</h3>
+        <section id="work" style="text-align: center">
+            <div >
+                <div >
+                    <h3>My latest work</h3>
                 </div>
-                <div class="col empty">&nbsp;</div>
-                <div class="col work-description">
-                    <p
-                            data-900="opacity:0;"
-                            data-1300="opacity:1;"
-                    >A selection of recent projects I've been working
+                <div style="text-align: center;font-size: 20px">
+                   A selection of recent projects I've been working
                         on: landing pages, websites, email marketing...</p>
                 </div>
-                <div class="col empty">&nbsp;</div>
-            </div>
-            <div class="work-examples page">
-                <div class="col work-item"
-                     data-1300="opacity:0;"
-                     data-1400="opacity:1;"
-                >
-                    <img src="/static/img/display/1.jpg" alt="">
-                    <div class="work-item-inside">
-                        <div class="work-item-inside-content">
-                            <h4>Design</h4>
-                            <a href="#" class="but">Click here</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col work-item"
-                     data-1300="opacity:0;"
-                     data-1400="opacity:1;"
-                >
-                    <img src="/static/img/display/1.jpg" alt="">
-                    <div class="work-item-inside">
-                        <div class="work-item-inside-content">
-                            <h4>Design</h4>
-                            <a href="#" class="but">Click here</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col work-item"
-                     data-1300="opacity:0;"
-                     data-1600="opacity:1;"
-                >
-                    <img src="/static/img/display/1.jpg" alt="">
-                    <div class="work-item-inside">
-                        <div class="work-item-inside-content">
-                            <h4>Design</h4>
-                            <a href="#" class="but">Click here</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col work-item"
-                     data-1300="opacity:0;"
-                     data-1400="opacity:1;"
-                >
-                    <img src="/static/img/display/1.jpg" alt="">
-                    <div class="work-item-inside">
-                        <div class="work-item-inside-content">
-                            <h4>Design</h4>
-                            <a href="#" class="but">Click here</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col work-item"
-                     data-1300="opacity:0;"
-                     data-1500="opacity:1;"
-                >
-                    <img src="/static/img/display/1.jpg" alt="">
-                    <div class="work-item-inside">
-                        <div class="work-item-inside-content">
-                            <h4>Design</h4>
-                            <a href="#" class="but">Click here</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col work-item"
-                     data-1300="opacity:0;"
-                     data-1600="opacity:1;"
-                >
-                    <img src="/static/img/display/1.jpg" alt="">
-                    <div class="work-item-inside">
-                        <div class="work-item-inside-content">
-                            <h4>Design</h4>
-                            <a href="#" class="but">Click here</a>
-                        </div>
-                    </div>
-                </div>
             </div>
         </section>
-        <hr>
 
         <!--页尾-->
-        <section id="contact" class="row">
-            <div class="contact-content page">
-                <div class="col contact-title">
-                    <h3
-                            data-100="-webkit-transform: translateX(100px); opacity:0;"
-                            data-500="-webkit-transform: translateX(0px); opacity:1;"
-                    >Get in touch</h3>
+        <section style="text-align: center">
+            <div >
+                <div style="font-size: 30px" >
+                    Get in touch</h3>
                 </div>
-                <div class="col empty">&nbsp;</div>
-                <div class="col contact-description">
+                <div c>
                     <a href="#" class="but">rosiwang@163.com</a>
-                </div>
-                <div class="col empty">&nbsp;</div>
-                <div class="col span_12 social">
-                    <a class="icon-twitter" href="#" target="_blank"></a>
-                    <a class="icon-github" href="#" target="_blank"></a>
-                    <a class="icon-instagram" href="#" target="_blank"></a>
-                    <a class="icon-linkedin" href="#" target="_blank"></a>
                 </div>
             </div>
         </section>
 
-        <footer id="footer" class="row">
-            <div class="footer-content page">
-                <div class="col f1">
+        <footer style="text-align: center" >
+            <div >
+                <div >
                     <p>
                         © 2018 Rosi Blogs. Welcome to contact me.
                        <br> Email : rosiwang@163.com <br>
@@ -187,15 +74,13 @@
                 </div>
             </div>
         </footer>
-    </div>
+
     </div>
 
 </template>
 
 <script>
     import $ from 'jquery'
-
-
     export default {
         name: ""
     }
@@ -229,5 +114,54 @@
 
 </script>
 <style scoped>
-    @import "/static/style/main.css";
+    /*#home {*/
+        /*background-color: rgba(0,0,0, .5);*/
+        /*background-image: url(/static/img/header-bg.jpg);*/
+        /*background-position: bottom;*/
+        /*background-repeat: no-repeat;*/
+        /*background-size: cover;*/
+        /*background-attachment: fixed;*/
+        /*position: relative;*/
+        /*text-align: center;*/
+        /*height: 540px;*/
+    /*}*/
+
+    .but {
+        display: inline-block;
+        font-family: 'Lato', Helvetica, Arial, sans-serif;
+        font-weight: 300;
+        font-size: 1rem;
+        font-size: 16px;
+        padding: 0 45px;
+        line-height: 55px;
+        background-color: #F10061;
+
+        -webkit-border-radius: 45px;
+        -moz-border-radius: 45px;
+        border-radius: 45px;
+
+        -webkit-background-clip: padding-box;
+        -moz-background-clip: padding-box;
+        background-clip: padding-box;
+
+        color: #fff;
+        border: 0;
+
+        -webkit-transition: background-color 0.3s ease;
+        -moz-transition: background-color 0.3s ease;
+        -o-transition: background-color 0.3s ease;
+        transition: background-color 0.3s ease;
+    }
+
+    .footer h3 {
+        font-family: 'Lato', Helvetica, Arial, sans-serif;
+        font-size: 2.25rem;
+        font-size: 27px;
+        font-weight: 400;
+        text-transform: uppercase;
+        letter-spacing: 3px;
+        margin-bottom: 45px;
+        color: #000;
+    }
+
 </style>
